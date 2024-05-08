af195407e74fc2f7bbc9e1d9178ec123931334bf
JacobCarriere <jacob2000@live.com>
2024-05-08 12:17:23 -0400

    added level based high score variables

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,4 +3,8 @@ extends Node
 var player_current_attack = false
 var coins = 0
 var score = 0
+var current_level = "none"
+var high_score_1 = 0
+var high_score_2 = 0
+var high_score_3 = 0
 var health = 100
