5310d491f82106689c946ec0118fe7cc7b7cfb69
JacobCarriere <jacob2000@live.com>
2024-05-08 11:09:12 -0400

    Level 2 Changes

---
+++ scenes/level_2.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var enemy_slime = preload("res://scenes/slime_enemy.tscn")
+var enemy_skeleton = preload("res://scenes/enemy_skeleton_base.tscn")
+var enemy_ghost = preload("res://scenes/ghost_enemy.tscn")
+var spawners = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
