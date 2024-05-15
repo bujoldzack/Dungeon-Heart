40ead478280a8818622de9ef414a04df9bc9efcf
bujoldzack <zbujold_26@hotmail.ca>
2024-05-15 15:22:15 -0400

    main menu connection

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -25,3 +25,6 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
